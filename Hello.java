8a68f811c6a0e67bfe226392b3913734bbee3b47
Seyoun Yoon <2271394@hansung.ac.kr>
2023-11-26 10:35:12 +0000

    Second Commit

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world from GIT!");
+        System.out.println("원격으로 푸시한 후 변경 사항!");
     }
 }
 

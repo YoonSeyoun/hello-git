666656db7ad12fe680009e47f61b20754017fb8e
Seyoun Yoon <2271394@hansung.ac.kr>
2023-11-26 11:26:04 +0000

    C4: modify hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world from GIT!");
         System.out.println("원격으로 푸시한 후 변경 사항!");
+        System.out.println("Changes after Pushing to remote!");
     }
 }
 
